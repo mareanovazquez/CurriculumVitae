78504baaec884567027358f3a7808408b676fafa
Mariano Vazquez <mareanovazquez@protonmail.com>
2023-08-16 10:27:57 -0300

    aplicación de decoración al componente AboutMe

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -7,6 +7,21 @@ export const AboutMe = () => {
                     <div className="card-header text-center display-6">
                         SOBRE MÍ
                     </div>
+                    <div className="contenidoSobreMi">
+                        <div className="card-body biografia">
+                            <p className="card-text"> Mi nombre es Mariano Dario Vazquez y soy oriundo de Santa Teresita.
+                                Me recibí como periodista en la Facultad de Periodismo y Comunicación Social (FPyCS) de
+                                la UNLP. Trabajé en medios gráficos, radiales y digitales de La Plata.
+                                En la actualidad me desempeño como docente de grado y posgrado en la FPyCS y como desarrollador web en la SCBA</p>
+                        </div>
+                        <div className="datosPerfil">
+                            <p> <span>EDAD:</span> 42 años </p>
+                            <p> <span>ESTADO CIVIL:</span> Soltero</p>
+                            <p> <span>UBICACIÓN:</span>CABA</p>
+                            <p><span>DISPONIBILIDAD:</span>Freelance</p>
+                           
+                        </div>
+                    </div>
                     <div className="card-body">
                         <p className="card-text"> Mi nombre es Mariano Dario Vazquez y soy oriundo de Santa Teresita.
                             Me recibí como periodista en la Facultad de Periodismo y Comunicación Social (FPyCS) de
@@ -15,6 +30,7 @@ export const AboutMe = () => {
                     </div>
                 </div>
             </div>
+
         </>
     )
 }

2e84394401e5e030cabb5360f6407b261862b389
mareanovazquez <mareanovazquez@protonmail.com>
2023-10-25 13:20:19 -0300

    ajustes en la animanción del título de la tarjeta

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -8,31 +8,22 @@ export const AboutMe = () => {
                     SOBRE MÍ
                 </div>
                 <div className="card">
-
                     <div className="contenidoSobreMi">
                         <div className="card-body biografia">
                             <p className="card-text"> Mi nombre es Mariano Dario Vazquez y soy oriundo de Santa Teresita.
                                 Me recibí como periodista en la Facultad de Periodismo y Comunicación Social (FPyCS) de
                                 la UNLP. Trabajé en medios gráficos, radiales y digitales de La Plata.
-                                En la actualidad me desempeño como docente de grado y posgrado en la FPyCS y como desarrollador web en la SCBA</p>
+                                En la actualidad me desempeño como docente de grado y posgrado en la FPyCS y como desarrollador web en la SCBA.</p>
                         </div>
                         <div className="datosPerfil">
                             <p> <span>EDAD:</span> 42 años </p>
                             <p> <span>ESTADO CIVIL:</span> Soltero</p>
                             <p> <span>UBICACIÓN:</span>CABA</p>
                             <p><span>DISPONIBILIDAD:</span>Freelance</p>
-
                         </div>
-                    </div>
-                    <div className="card-body">
-                        <p className="card-text"> Mi nombre es Mariano Dario Vazquez y soy oriundo de Santa Teresita.
-                            Me recibí como periodista en la Facultad de Periodismo y Comunicación Social (FPyCS) de
-                            la UNLP. Trabajé en medios gráficos, radiales y digitales de La Plata.
-                            En la actualidad me desempeño como docente de grado y posgrado en la FPyCS y como desarrollador web en la SCBA</p>
                     </div>
                 </div>
             </div>
-
         </>
     )
 }

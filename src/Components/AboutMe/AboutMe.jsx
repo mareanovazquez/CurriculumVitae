0e10f1b5a5a08b39985caa434bb860b65c4b4478
Mariano Vázquez <mareanovazquez@protonmail.com>
2024-04-27 14:55:19 -0300

    cambios en texto biografía y datos perfil

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -9,10 +9,16 @@ export const AboutMe = () => {
 
                 <div className="contenidoSobreMi">
                     <div className=" biografia">
-                        <p className="textoBiografia"> Mi nombre es Mariano Dario Vazquez y soy oriundo de Santa Teresita.
-                            Me recibí como periodista en la Facultad de Periodismo y Comunicación Social (FPyCS) de
-                            la UNLP. Trabajé en medios gráficos, radiales y digitales de La Plata.
-                            En la actualidad me desempeño como docente de grado y posgrado en la FPyCS, en una mesa de ayuda en el área de informática de la Suprema Corte Buenos Aires (SCBA) y también soy el responsable de desarrollo web de los proyectos de <a href="https://estudiorec.com.ar" target="_blank">ESTUDIO REC</a>.</p>
+                        <p className="textoBiografia primerParrafo"> Mi nombre es Mariano Darío Vázquez, oriundo de Santa Teresita, provincia de Buenos Aires. Soy Doctor en Comunicación y Licenciado en Comunicación Social, egresado de la Facultad de Periodismo y Comunicación Social (FPyCS) de la Universidad Nacional de La Plata (UNLP).
+                            Poseo experiencia laboral en medios gráficos, radiales y digitales de la ciudad de La Plata.</p>
+                        <div className="textoBiografia segundoParrafo"> <p>Actualmente me desempeño en diversos roles:</p>
+                            <ul>
+                                <li>Docente de grado y posgrado en la FPyCS de la UNLP.</li>
+                                <li>Integrante del equipo de mesa de ayuda en el área de informática de la SCBA.</li>
+                                <li>Responsable de desarrollo web en <a href="https://estudiorec.com.ar" target="_blank">ESTUDIO REC</a> </li>
+                                </ul>
+                        </div>
+                        <p className="textoBiografia">A lo largo de mi trayectoria académica y profesional, he adquirido sólidos conocimientos y habilidades en comunicación, periodismo, docencia e investigación, además de desarrollo web. Me destaco por mi capacidad de trabajo en equipo, adaptabilidad y compromiso en los proyectos que asumo. </p>
                     </div>
                     <div className="datosPerfil">
                         <p> <span>EDAD:</span> 43 años </p>

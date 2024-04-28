c091705905eade87087cf75483abf47185e83b97
Mariano Vázquez <mareanovazquez@protonmail.com>
2024-04-27 23:10:24 -0300

    cambios en el texto de biografía

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ export const AboutMe = () => {
 
                 <div className="contenidoSobreMi">
                     <div className=" biografia">
-                        <p className="textoBiografia primerParrafo"> Mi nombre es Mariano Darío Vázquez, oriundo de Santa Teresita, provincia de Buenos Aires. Soy Doctor en Comunicación y Licenciado en Comunicación Social, egresado de la Facultad de Periodismo y Comunicación Social (FPyCS) de la Universidad Nacional de La Plata (UNLP).
+                        <p className="textoBiografia primerParrafo"> Mi nombre es Mariano Darío Vazquez y crecí junto al mar, en Santa Teresita, provincia de Buenos Aires. Soy Doctor en Comunicación y Licenciado en Comunicación Social, egresado de la Facultad de Periodismo y Comunicación Social (FPyCS) de la Universidad Nacional de La Plata (UNLP).
                             Poseo experiencia laboral en medios gráficos, radiales y digitales de la ciudad de La Plata.</p>
                         <div className="textoBiografia segundoParrafo"> <p>Actualmente me desempeño en diversos roles:</p>
                             <ul>
@@ -18,7 +18,7 @@ export const AboutMe = () => {
                                 <li>Responsable de desarrollo web en <a href="https://estudiorec.com.ar" target="_blank">ESTUDIO REC</a> </li>
                                 </ul>
                         </div>
-                        <p className="textoBiografia">A lo largo de mi trayectoria académica y profesional, he adquirido sólidos conocimientos y habilidades en comunicación, periodismo, docencia e investigación, además de desarrollo web. Me destaco por mi capacidad de trabajo en equipo, adaptabilidad y compromiso en los proyectos que asumo. </p>
+                        <p className="textoBiografia">A lo largo de mi trayectoria académica y profesional, adquirí conocimientos y desarrollé habilidades en comunicación, periodismo, docencia e investigación y <i>web deb</i>. Me siento cómodo trabajando en equipo, me adapto fácilmente a nuevos entornos y tengo un alto compromiso para con los proyectos que asumo. </p>
                     </div>
                     <div className="datosPerfil">
                         <p> <span>EDAD:</span> 43 años </p>
